7bcf9605f0c120f35f3a4668b606d2b79c57a180
Ivo Roberto <byivo@byivo-personal.byivo.rocks>
2016-09-09 17:00:34 -0300

    changes in exceptions types

---
+++ src/rocks/byivo/exec2/OutOfRange.java
@@ -0,0 +1,14 @@
+package rocks.byivo.exec2;
+
+public class OutOfRange extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OutOfRange(Integer outRangedInput) {
+		super(String.format("Por favor, informe apenas um múltiplo de 10 entre 10 e 1000.", outRangedInput));
+	}
+
+}
